07d18500d50973482b2494dd6e91978f9cb75f5a
mb1 <matthew.beckers@roke.co.uk>
2016-10-27 17:32:20 +0100

    updated example to print ports for each packet

--- pcapreader.py
+++ pcapreader.py
@@ -7,3 +7,9 @@ pcap = dpkt.pcap.Reader(f)
 
 for ts, buf in pcap:
     print ts, len(buf)
+    eth = dpkt.ethernet.Ethernet(buf)
+    if isinstance(eth.data, dpkt.ip.IP):
+        ip = eth.data
+        if isinstance(ip.data, dpkt.tcp.TCP):
+            tcp = ip.data
+            print tcp.dport
